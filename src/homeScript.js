bc669cd577a2da598175de9b99a905ab80d9dd39
Tejas Paruthooli <contej215@gmail.com>
2018-12-06 17:22:23 -0500

    Updated the date sections
    Popup window appears if departure is after the arrival

--- src/homeScript.js
+++ src/homeScript.js
@@ -1,7 +1,22 @@
 var loginRoot = "http://comp426.cs.unc.edu:3001/api/";
 
+var date1;
+var date2;
+
 $(document).ready(() => {
    //Functions go here
    console.log("hello");
+   $('#gobutton').click(function () {
+        date1 = $('#datepicker1').val();
+        console.log($('#datepicker1').val())
+        console.log($('#datepicker2').val())
+        date2 = $('#datepicker2').val();
+        console.log(date1 < date2);
+        if(date1 > date2) {
+            alert("Departure Date Must Be Earlier Than Arrival Date");
+        } else {
+            
+        }
+   });
 });
 
